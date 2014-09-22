d87502ccf977d4c30d4b529cd49dadd9c48b11b8
juliejk <juliejk92@gmail.com>
2014-09-22 15:47:34 +0200

    Addein the create table statements.

--- sql/createDB.sql
+++ sql/createDB.sql
@@ -1,1 +1,33 @@
 -- script to create the database for the sensor data
+
+CREATE TABLE LightSample
+(
+SampleID int,
+SensorID int,
+Value double,
+Date date
+);
+
+CREATE TABLE TemperatureSample
+(
+SampleID int,
+SensorID int,
+Value double,
+Date date
+);
+
+CREATE TABLE AccelerationSample
+(
+SampleID int,
+SensorID int,
+Value double,
+Date date
+);
+
+CREATE TABLE HumiditySample
+(
+SampleID int,
+SensorID int,
+Value double,
+Date date
+);
