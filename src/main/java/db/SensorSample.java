90ee88e5f89a6c076e898318c9bbf8e68a72cfb0
Audun Sutterud <audunx@gmail.com>
2014-09-26 08:43:10 +0200

    Add fields, getters, and a default constructor.

---
+++ src/main/java/db/SensorSample.java
@@ -0,0 +1,45 @@
+package db;
+
+import java.time.LocalDateTime;
+
+/**
+ * A wrapper class for all information of a single sensor sample in time.
+ *
+ * Created by audun on 9/26/14.
+ */
+public class SensorSample {
+
+    private int sampleID;
+    private int sensorID;
+    private double value;
+    private LocalDateTime dateTime;
+    private String type;
+
+    public SensorSample(String type, int sampleID, int sensorID, double value, LocalDateTime dateTime) {
+        this.type = type;
+        this.sampleID = sampleID;
+        this.sensorID = sensorID;
+        this.value = value;
+        this.dateTime = dateTime;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getSampleID() {
+        return sampleID;
+    }
+
+    public int getSensorID() {
+        return sensorID;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+}
