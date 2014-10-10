f1012a0bbe5bd2802b1f99dc807e39e6287ac78c
juliejk <juliejk92@gmail.com>
2014-10-10 10:46:06 +0200

    Humidity should ish work now

--- Visualiser/src/Interface/HumidityRender.java
+++ Visualiser/src/Interface/HumidityRender.java
@@ -13,13 +13,27 @@ public class HumidityRender implements Renderer {
         if (instruction instanceof HumidityInstruction) {
             double x = ((HumidityInstruction) instruction).getX();
             double y = ((HumidityInstruction) instruction).getY();
+            double humidity = ((HumidityInstruction) instruction).getHumidity();
             GraphicsContext graphicsContext = ((HumidityInstruction) instruction).getCanvas().getGraphicsContext2D();
             graphicsContext.setStroke(Color.BLUE);
             graphicsContext.beginPath();
             graphicsContext.moveTo(x, y);
             graphicsContext.bezierCurveTo(x+30, y+35, x-30, y+35, x, y);
-            double white = 0.5;
-            double blue = 1;
+            double white;
+            double blue;
+            if (humidity < 50){
+                white = 1;
+                blue = 1;
+            }
+            else if(humidity > 50){
+                white = 0;
+                blue = 0;
+            }
+            else{
+                white = 0.5;
+                blue = 1;
+            }
+
             graphicsContext.setFill(new LinearGradient(1, 0, 1, 1, true,
                     CycleMethod.NO_CYCLE,
                     new Stop(white, Color.WHITE),
