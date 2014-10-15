80b9e5cad3e29c1eb4112e0c093926aa2a0e076e
Audun Sutterud <audunx@gmail.com>
2014-10-15 08:46:57 +0200

    Removed files merged to this branch in error

--- README.md
+++
@@ -1,4 +0,0 @@
-tdt4290g10
-==========
-
-Private code repository for group 10, TDT4290 - Customer Driven Project.
--- sql/queries.sql
+++
@@ -1,7 +0,0 @@
--- this file contains useful database queries
-SELECT DISTINCT * FROM (
-	SELECT MAX(Date) AS MaxDate, SensorID
-	FROM TemperatureSample
-	GROUP BY SensorID
-) AS T1 JOIN TemperatureSample AS T2
-ON MaxDate = Date AND T1.SensorID = T2.SensorID;
